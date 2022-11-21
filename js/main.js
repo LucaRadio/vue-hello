33fafda2bb227b1bcbba432eb680fd1e4ef063d4
LucaRadio <luca.1407@hotmail.it>
2022-11-21 14:01:06 +0100

    Complete Exercise

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const { createApp } = Vue;
+
+
+
+
+createApp({
+    data() {
+        return {
+            titleMain: "Hello World!",
+            classes: "text-center text-white",
+            pic: "https://picsum.photos/id/1/300/300"
+        }
+    },
+    methods: {
+        changeImgClick() {
+            this.pic = `https://picsum.photos/id/${this.randomNumGenerator(1, 300)}/300/300`
+        },
+        randomNumGenerator(min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        },
+
+    }
+}).mount("#app")
